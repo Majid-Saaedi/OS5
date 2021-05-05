384bf447c911cdde0a5243eb41a755483ec6c018
Majid-Saaedi <majidsaedi378.ms@gmail.com>
2021-05-06 00:18:48 +0430

    OS5

---
+++ Q1.sh
@@ -0,0 +1,2 @@
+git config --global user.name "Github User Name"
+git config --global user.email "Github Email"
---
+++ Q3.sh
@@ -0,0 +1,6 @@
+git init
+git add --all
+git commit -m "commit"
+git branch -M main
+git remote add origin (repository URL)
+git push -u origin main
